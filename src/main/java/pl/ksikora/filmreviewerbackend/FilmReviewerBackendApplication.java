9d8ff481608f79646cd00bfd004679607204e5cd
krystian-sikora <s223097@student.uek.krakow.pl>
2023-12-29 23:28:09 +0100

    initial commit

---
+++ src/main/java/pl/ksikora/filmreviewerbackend/FilmReviewerBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.ksikora.filmreviewerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmReviewerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmReviewerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ksikora/filmreviewerbackend/FilmReviewerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ksikora.filmreviewerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmReviewerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
